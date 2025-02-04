f86db792d09d53a6727ce56196f2a350142ac401
Sergey <molotov9500@gmail.com>
2025-02-04 15:07:28 +0300

    init

---
+++ src/main/java/org/sber/yetanotherchat/YetAnotherChatApplication.java
@@ -0,0 +1,13 @@
+package org.sber.yetanotherchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sber/yetanotherchat/YetAnotherChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sber.yetanotherchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
